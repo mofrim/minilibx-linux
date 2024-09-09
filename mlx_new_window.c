2193f33dea14a556f96ec80da507c68a8f003b8d
mofrim <mofrim@posteo.de>
2024-09-09 09:44:16 +0200

    remove anti_resize -> window opens on hyprland

--- mlx_new_window.c
+++ mlx_new_window.c
@@ -36,11 +36,26 @@ void	*mlx_new_window(t_xvar *xvar,int size_x,int size_y,char *title)
 	xswa.event_mask = 0xFFFFFF;	/* all events */
 	if (!(new_win = malloc(sizeof(*new_win))))
 		return ((void *)0);
-	new_win->window = XCreateWindow(xvar->display,xvar->root,0,0,size_x,size_y,
-					0,CopyFromParent,InputOutput,xvar->visual,
-					CWEventMask|CWBackPixel|CWBorderPixel|
-					CWColormap,&xswa);
-	mlx_int_anti_resize_win(xvar,new_win->window,size_x,size_y);
+	new_win->window = XCreateWindow(
+			xvar->display,
+			xvar->root,
+			0,
+			0,
+			size_x,
+			size_y,
+			0,
+			CopyFromParent,
+			InputOutput,
+			xvar->visual,
+			CWEventMask|CWBackPixel|CWBorderPixel|
+			CWColormap,
+			&xswa);
+	/*
+	 * FIX after removing this, window would even show on way/hyprland
+	 * TODO understand
+	 *
+	 * mlx_int_anti_resize_win(xvar,new_win->window,size_x,size_y); */
+
 	XStoreName(xvar->display,new_win->window,title);
 	XSetWMProtocols(xvar->display, new_win->window, &(xvar->wm_delete_window), 1);
 	xgcv.foreground = -1;
